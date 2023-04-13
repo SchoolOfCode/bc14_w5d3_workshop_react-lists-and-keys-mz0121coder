b1c8c11267d1579a0f33ac5cfac40707d3eb4685
Mohammad Zafar <mzcoder7@gmail.com>
2023-04-13 18:47:15 +0100

    render posts with main props

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import "./index.css";
+import './index.css';
 
-import samplePosts from "../../libs/data";
+import samplePosts from '../../libs/data';
 
-import Post from "../Post";
+import Post from '../Post';
 
 function Main() {
-  const [posts, setPosts] = useState(samplePosts);
+	const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+	return (
+		<main id='main'>
+			{posts.map(post => (
+				<Post
+					key={post.postId}
+					title={post.title}
+					date={post.date}
+					author={post.author}
+					text={post.text}
+					highlights={post.highlights}
+					image={post.image}
+				/>
+			))}
+		</main>
+	);
 }
 
 export default Main;
