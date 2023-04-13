3af95abe9ff10fc1ad8909d70ae760dccaa312ca
Mohammad Zafar <mzcoder7@gmail.com>
2023-04-13 18:38:07 +0100

    render posts with title, text, highlights + image

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import "./index.css";
+import './index.css';
 
-function Post() {
-  //Replace the word "test" with your post content!
-  return <article>test</article>;
+function Post(props) {
+	//Replace the word "test" with your post content!
+	return (
+		<article>
+			<h1>{props.title}</h1>
+			<p>{props.date}</p>
+			<p>{props.author}</p>
+			<p>{props.text}</p>
+			<ul>
+				{props.highlights.map((highlight, index) => (
+					<li key={index}>{highlight}</li>
+				))}
+			</ul>
+			<img
+				src={props.image.link}
+				alt={props.image.alt}
+				style={{ width: 200, height: 200 }}
+			/>
+		</article>
+	);
 }
 
 export default Post;
